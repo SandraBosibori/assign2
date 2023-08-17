d1066b87a2cbc137b9d7a7c06be519c0f0b40eb3
SandraBosibori <sandrahbosibori@gmail.com>
2023-08-17 16:22:13 +0300

    initial push

--- src/app/shops/page.js
+++ src/app/shops/page.js
@@ -2,17 +2,16 @@ import TopMost from '@/components/topMost'
 import Nav from '@/components/nav'
 import Search from '@/components/search'
 import SideNav from '@/components/sideNav'
-import Front from '@/components/front'
 
-import styles from './page.module.css'
+
+import styles from '../page.module.css'
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import Right from '@/components/right'
 
 
-export default function Home() {
+export default function Assignees() {
   return (
     <>
     <TopMost/>
@@ -20,9 +19,7 @@ export default function Home() {
     <Search/>
     <div className={styles.mainpage}>
       <SideNav/>
-      <Front/>
-      <Right/>
-
+      
     </div>
     </>
   )
